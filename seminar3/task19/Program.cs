0547cec84f59c38e2f52ee12f5fea2f763b12a0e
Mr_Gretsov <mrgretsov@yandex.ru>
2022-12-04 16:51:45 +0300

    added task info

--- seminar3/task19/Program.cs
+++ seminar3/task19/Program.cs
@@ -2,3 +2,14 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == )
+{
+    Console.WriteLine("третьей цифры нет");
+}
+while (number >= 999)
+{
+    number = number / 10;
+}
+Console.WriteLine("третья цифра вводимого числа: " + number %10);
