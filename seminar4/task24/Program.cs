5afd204c4f1b45724cfee9043ca3e8b5d2135094
Mr_Gretsov <mrgretsov@yandex.ru>
2022-12-05 21:12:11 +0300

    added new task

--- seminar4/task24/Program.cs
+++ seminar4/task24/Program.cs
@@ -2,3 +2,12 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+Console.Write("Введите число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = 0;
+while(number_1 >= 0)
+{
+    number_2 +=number_1;
+    number_1 -= 1;
+}
+Console.WriteLine(number_2);
