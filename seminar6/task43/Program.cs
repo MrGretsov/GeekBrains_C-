eddc3cfa0c0af55658b789977ddd24a8a90a32b6
Mr_Gretsov <mrgretsov@yandex.ru>
2022-12-15 19:56:22 +0300

    added new tasks

---
+++ seminar6/task43/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+string X(int k1, int k2, int b1, int b2)
+{
+    string result = string.Empty;
+    int x = 0;
+    int y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+    result = $"({x}, {y})";
+    return result;
+}
+Console.WriteLine(X(2, -3, -3, 2));
