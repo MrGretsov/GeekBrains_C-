3a2bcabf328733abb5767e4971816fcf3aa3bfaf
Mr_Gretsov <mrgretsov@yandex.ru>
2022-12-26 20:17:25 +0300

    added new seminar9

---
+++ seminar9/task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Программа выводит все натуральные числа между Стартовым и Конечным числом");
+Console.WriteLine("Введите cтартовое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число Конечное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+PrintNumberToN(numberN, numberM);
+
+void PrintNumberToN(int numberN, int numberM = 1)
+{
+    if (numberN == numberM)
+    {
+        Console.Write(numberN + " ");
+        return;
+    }
+    else if (numberN > numberM)
+    {
+        PrintNumberToN(numberN - 1, numberM);
+        Console.Write(numberN + " ");
+    }
+    else
+    {
+        PrintNumberToN(numberN + 1, numberM);
+        Console.Write(numberN + " ");
+    }
+}
---
+++ seminar9/task68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+Console.WriteLine("Программа выводит функцию Аккермана");
+Console.WriteLine("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(AkkermanFunction(numberM, numberN));
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 & n == 0)
+    {
+        return AkkermanFunction(m-1, 1);
+    }
+    else if (m > 0 & n > 0)
+     {
+        return AkkermanFunction(m-1, AkkermanFunction(m, n-1));
+    }
+
+    return 0;
+}
